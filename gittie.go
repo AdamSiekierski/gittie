97e6d0611716ff30ffed2cd5b34505f5178f10c6
Adam Siekierski <a@siekierski.ml>
2020-07-25 20:51:09 +0200

    Init with cli lib

--- gittie.go
+++ gittie.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"github.com/urfave/cli/v2"
 )
 
+func application(c *cli.Context) error {
+	fmt.Println("hello world!")
+	return nil
+}
+
 func main() {
-	fmt.Println("hello world")
+	app := &cli.App{
+		Name: "Gittie",
+		Usage: "Quickly create .gitignore in your project using one simple command ",
+		Action: application,
+	}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AdamSiekierski/gittie
 
 go 1.14
+
+require github.com/urfave/cli/v2 v2.2.0
