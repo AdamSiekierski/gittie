392a7bf3c16193db81d15e3d99aad6576f16db33
Adam Siekierski <a@siekierski.ml>
2020-07-25 21:44:05 +0200

    Basis of app structure

--- gittie.go
+++ gittie.go
@@ -5,23 +5,57 @@ import (
 	"os"
 	"log"
 	"github.com/urfave/cli/v2"
+	"github.com/AdamSiekierski/gittie/utils"
 )
 
 func application(c *cli.Context) error {
-	fmt.Println("hello world!")
+	if c.Bool("list") {
+		_, err := utils.GetTemplates()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if c.String("create") != "" {
+		fmt.Printf("hmmm %s", c.String("create"))
+	}
+
 	return nil
 }
 
 func main() {
 	app := &cli.App{
-		Name: "Gittie",
+		Name:  "Gittie",
 		Usage: "Quickly create .gitignore in your project using one simple command ",
-		Action: application,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "list",
+				Usage:   "Show all available templates",
+				Aliases: []string{"l"},
+				Value:   false,
+			},
+			&cli.StringFlag{
+				Name:    "template",
+				Usage:   "Generate a .gitignore from selected template",
+				Aliases: []string{"t"},
+				Value:   "",
+			},
+		},
+		HideHelpCommand: true,
 	}
 
-	err := app.Run(os.Args)
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("list") {
+			fmt.Println("list")
+		}
 
-	if err != nil {
-		log.Fatalln(err)
+		if c.String("template") != "" {
+			fmt.Printf("template %s", c.String("create"))
+		}
+
+		return nil
 	}
+
+	app.Run(os.Args)
 }
---
+++ utils/templates.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"fmt"
+)
+
+// GetTemplates : Get all .gitignore templates
+func GetTemplates() ([]string, error) {
+	fmt.Println("list templates")
+
+	return nil, nil
+}
